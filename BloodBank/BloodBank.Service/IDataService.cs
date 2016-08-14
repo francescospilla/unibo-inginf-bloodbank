8d6a5036ac3de130e13f5887d19226548fe51aad
Francesco Spilla <francesco.spilla@gmail.com>
2016-08-14 12:39:28 +0200

    Rimosso BloodBank.Service a seguito del refactoring

--- BloodBank/BloodBank.Service/IDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace BloodBank.Model.Service {
-
-    public interface IDataService<TModel> where TModel : class {
-        void AddModel(TModel model);
-        IEnumerable<TModel> GetModels();
-    }
-}
