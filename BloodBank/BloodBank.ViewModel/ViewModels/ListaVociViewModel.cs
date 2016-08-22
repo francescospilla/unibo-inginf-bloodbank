7e2f8e630fbfd6614c9a8dad641d06d4697d543a
ShurayukiHime <jos.7-11@hotmail.it>
2016-08-22 10:56:35 +0200

    meh

---
+++ BloodBank/BloodBank.ViewModel/ViewModels/ListaVociViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BloodBank.Core.Extensions;
+using BloodBank.Model;
+using BloodBank.Model.Donatori;
+using BloodBank.Model.Indagini;
+using BloodBank.Model.Service;
+using BloodBank.Model.Tests;
+using BloodBank.ViewModel.Components;
+using BloodBank.ViewModel.Service;
+using Stylet;
+
+namespace BloodBank.ViewModel.ViewModels
+{
+    public class ListaVociViewModel : ViewModel<ListaVoci>
+    {
+        public ListaVociViewModel(IEventAggregator eventAggregator, IModelValidator validator = null) : base(eventAggregator, validator)
+        {
+        }
+
+        public Idoneità Idoneità { get; set; }
+        public IEnumerable<Voce> Voci { get; set; }
+        public Donatore Donatore { get; set;  }
+
+        protected override void SyncModelToViewModel()
+        {
+            Idoneità = Model.Idoneità;
+            Voci = Model.Voci;
+            Donatore = Model.Donatore;
+        }
+    }
+}
