56b3adbb5b72b374b1ed0be70af1f76cd490db1f
Francesco Spilla <francesco.spilla@gmail.com>
2016-09-13 15:39:03 +0200

    Partial commit

--- BloodBank/BloodBank.ViewModel/ShellViewModel.cs
+++ BloodBank/BloodBank.ViewModel/ShellViewModel.cs
@@ -17,7 +17,7 @@ namespace BloodBank.ViewModel {
     [ImplementPropertyChanged]
     public class ShellViewModel : Conductor<NavigationMenuItem>.Collection.OneActive, IHandle<NavMenuEvent> {
 
-        public ShellViewModel(IEventAggregator eventAggregator, DonatoriViewModel donatoriViewModel, DonazioniViewModel donazioniViewModel, ListeIndaginiQuestionarioViewModel listeIndaginiQuestionarioViewModel, VisiteMedicheViewModel visiteMedicheViewModel, ListaVociViewModel listaVociViewModel) {
+        public ShellViewModel(IEventAggregator eventAggregator, DonatoriViewModel donatoriViewModel, DonazioniViewModel donazioniViewModel, ListeIndaginiQuestionarioViewModel listeIndaginiQuestionarioViewModel, VisiteMedicheViewModel visiteMedicheViewModel, ListaVociViewModel listaVociViewModel, NewListaVociViewModel newListaVociViewModel) {
             eventAggregator.Subscribe(this);
 
             DisplayName = "BloodBank";
@@ -35,6 +35,7 @@ namespace BloodBank.ViewModel {
             Items.Add(new NavigationMenuItem("Questionari", "Heart", listeIndaginiQuestionarioViewModel));
             Items.Add(new NavigationMenuItem("Visite Mediche", "Hospital", visiteMedicheViewModel));
             Items.Add(new NavigationMenuItem("Tests", "Help", listaVociViewModel));
+            Items.Add(new NavigationMenuItem("NewListaVoci", "Help", newListaVociViewModel));
         }
 
         #region NavMenu
---
+++ BloodBank/BloodBank.ViewModel/ViewModels/NewListaVociViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using BloodBank.Model.Models.Persone;
+using BloodBank.ViewModel.Service;
+using Stylet;
+
+namespace BloodBank.ViewModel.ViewModels {
+    public class NewListaVociViewModel : Screen {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IDataService<Donatore, DonatoreViewModel> _donatoreDataService;
+
+
+        #region Constructors
+
+        public NewListaVociViewModel(IEventAggregator eventAggregator, IDataService<Donatore, DonatoreViewModel> donatoreDataService) {
+            _eventAggregator = eventAggregator;
+            _donatoreDataService = donatoreDataService;
+
+            DonatoreEnumerable = _donatoreDataService.GetViewModels();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public DonatoreViewModel SelectedDonatore { get; set; }
+
+        #endregion
+
+        public IEnumerable<DonatoreViewModel> DonatoreEnumerable { get; }
+    }
+}
