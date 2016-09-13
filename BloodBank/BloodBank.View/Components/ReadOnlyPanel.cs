3e56e0eb9c3fec6ffda8ffc02b32f7dc64dc667d
Francesco Spilla <francesco.spilla@gmail.com>
2016-09-13 15:35:17 +0200

    Aggiunto ReadOnlyPanel

---
+++ BloodBank/BloodBank.View/Components/ReadOnlyPanel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using ComboBox = System.Windows.Controls.ComboBox;
+using TextBox = System.Windows.Controls.TextBox;
+
+namespace BloodBank.View.Components {
+    public class ReadOnlyPanel {
+        public static readonly DependencyProperty IsReadOnlyProperty =
+            DependencyProperty.RegisterAttached(
+                "IsReadOnly", typeof(bool), typeof(ReadOnlyPanel),
+                new FrameworkPropertyMetadata(false,
+                    FrameworkPropertyMetadataOptions.Inherits, ReadOnlyPropertyChanged));
+
+        public static bool GetIsReadOnly(DependencyObject o) {
+            return (bool)o.GetValue(IsReadOnlyProperty);
+        }
+
+        public static void SetIsReadOnly(DependencyObject o, bool value) {
+            o.SetValue(IsReadOnlyProperty, value);
+        }
+
+        private static void ReadOnlyPropertyChanged(
+            DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            if (o.GetType().GetProperty("IsEnabled") != null) {
+                o.GetType().GetProperty("IsEnabled").SetValue(o, !(bool)e.NewValue);
+            }
+
+        }
+    }
+}
