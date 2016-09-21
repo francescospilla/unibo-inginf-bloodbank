2600b93e1f383248b5ce82d10b2c83e2fe4d0e19
Francesco Spilla <francesco.spilla@gmail.com>
2016-09-21 17:20:25 +0200

    Rimossi Donatori NonIdonei in VisitaMedicaViewModel

--- BloodBank/BloodBank.ViewModel/Validation/Tests/VisitaMedicaValidator.cs
+++ BloodBank/BloodBank.ViewModel/Validation/Tests/VisitaMedicaValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using BloodBank.Model.Models;
 using BloodBank.Model.Models.Tests;
 using BloodBank.ViewModel.ViewModels.Tests;
 using FluentValidation;
@@ -7,18 +8,19 @@ namespace BloodBank.ViewModel.Validation.Tests
 {
     public class VisitaMedicaValidator : AbstractValidator<VisitaMedicaViewModel>
     {
-        public VisitaMedicaValidator()
-        {
-            RuleFor(vm => vm.Donatore).NotEmpty();
+        public VisitaMedicaValidator() {
+            RuleFor(vm => vm.Donatore)
+                .NotEmpty()
+                .Must(donatore => donatore?.Idoneità != Idoneità.NonIdoneo)
+                .When(vm => !vm.IsInitialized);
             RuleFor(vm => vm.Medico).NotEmpty();
             RuleFor(vm => vm.DescrizioneBreve).NotEmpty();
             RuleFor(vm => vm.Idoneità).NotNull().IsInEnum();
             RuleFor(vm => vm.DataOra).GreaterThan(vm =>
             vm.Donatore.ListaTest.Last().Data).When(vm => {
                 if (vm.IsInitialized) return false;
-                if (vm.Donatore == null) return false;
 
-                Test lastTest = vm.Donatore.ListaTest.LastOrDefault();
+                Test lastTest = vm.Donatore?.ListaTest.LastOrDefault();
                 if (lastTest == null) return false;
 
                 return true;
