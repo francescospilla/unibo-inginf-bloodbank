555d16190e64e43e438abe3763445c6b8ed1483b
Francesco Spilla <francesco.spilla@gmail.com>
2016-09-25 20:36:08 +0200

    Fix GUI

--- BloodBank/BloodBank.Mock/Indagini/ListaIndaginiAnalisiMockDataService.cs
+++ BloodBank/BloodBank.Mock/Indagini/ListaIndaginiAnalisiMockDataService.cs
@@ -13,7 +13,7 @@ namespace BloodBank.Mock.Indagini {
         public ListaIndaginiAnalisiMockDataService(IndagineAnalisiMockDataService ia) {
             Lia1 = new ListaIndagini<Analisi>("Analisi parziali 1") { ia.A1, ia.A2, ia.A3, ia.A4 };
             Lia2 = new ListaIndagini<Analisi>("Analisi complete") { ia.A1, ia.A2, ia.A3, ia.A4, ia.A5 };
-            Lia3 = new ListaIndagini<Analisi>("Analisi parziali 1") { ia.A1, ia.A2 };
+            Lia3 = new ListaIndagini<Analisi>("Analisi parziali 2") { ia.A1, ia.A2 };
 
             _models = new ObservableCollection<ListaIndagini<Analisi>> { Lia1, Lia2, Lia3 };
         }
